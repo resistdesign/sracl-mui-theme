1a26d9ea9d5d96c0685d87438a54c6903c2415a3
Ryan Graff <ryantgraff@resistdesign.com>
2020-02-23 10:31:44 -0500

    feat: (package) Add a demo application.

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import {hot} from 'react-hot-loader';
+import React, {Component} from 'react';
+import styled from 'styled-components';
+
+const Base = styled.div`
+  
+`;
+
+export class App extends Component {
+  static propTypes = {};
+
+  render() {
+    return (
+      <Base>
+
+      </Base>
+    );
+  }
+}
+
+export default hot(module)(App);
---
+++ src/index-entry.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './App';
+
+const root = document.getElementById('app-root');
+
+ReactDOM.render(<App/>, root);
